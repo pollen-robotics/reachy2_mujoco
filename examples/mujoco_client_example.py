dbc29870c5ab52bfb8dbe883713a0a694e9143c2
apirrone <awanegenebayo@gmail.com>
2024-12-18 16:20:40 +0100

    nothing

--- examples/mujoco_client_example.py
+++ examples/mujoco_client_example.py
@@ -5,10 +5,7 @@ from reachy2_mujoco import ReachySDK, CameraView
 
 reachy = ReachySDK("localhost")
 
-# reachy.mobile_base.goto(-1.0, 1.0, -np.pi / 2)
-
 reachy.l_arm.shoulder.roll.goal_position = 40
-# reachy.l_arm.elbow.pitch.goal_position = -90
 
 reachy.r_arm.shoulder.roll.goal_position = -40
 reachy.r_arm.elbow.pitch.goal_position = -90
@@ -16,10 +13,8 @@ while True:
     pos = 0.5 * np.sin(2 * np.pi * 0.2 * time.time())
 
     reachy.r_arm.shoulder.pitch.goal_position = np.rad2deg(pos)
-    # reachy.l_arm.shoulder.pitch.goal_position = np.rad2deg(pos)
 
-    gripper_opening = 100 if pos > 0 else 0
-    reachy.r_arm.gripper.set_opening(gripper_opening)
+    reachy.r_arm.gripper.set_opening(100 if pos > 0 else 0)
 
     reachy.head.neck.roll.goal_position = np.rad2deg(pos)
     reachy.head.neck.pitch.goal_position = np.rad2deg(pos)
--- reachy2_mujoco/reachy2_mujoco/parts/arm.py
+++ reachy2_mujoco/reachy2_mujoco/parts/arm.py
@@ -1,10 +1,17 @@
 from reachy2_mujoco.parts.joints import Shoulder, Elbow, Wrist, Gripper
+from reachy2_symbolic_ik.control_ik import ControlIK
+import os
 
 
 class Arm:
     def __init__(self, model, data, prefix="l_"):
         self._model = model
         self._data = data
+        self._prefix = prefix
+        self._urdf_path = "/".join(os.path.realpath(__file__).split("/")[:-3]) + "/description/modified_urdf/reachy2.urdf"
+        self._control_ik = ControlIK(urdf_path=self._urdf_path)
+
+        # self._control_ik.symbolic_ik_solver
 
         self.shoulder = Shoulder(self._model, self._data, prefix=prefix)
         self.elbow = Elbow(self._model, self._data, prefix=prefix)
@@ -18,7 +25,42 @@ class Arm:
         self.wrist._update()
         self.gripper._update()
 
+    def get_current_positions(self):
+        return [
+            self.shoulder.pitch.current_position,
+            self.shoulder.roll.current_position,
+            self.elbow.yaw.current_position,
+            self.elbow.pitch.current_position,
+            self.wrist.roll.current_position,
+            self.wrist.pitch.current_position,
+            self.wrist.yaw.current_position
+        ]
+
     # TODO implement
-    def goto(self):
-        print("Not implemented")
-        pass
+    def goto(self, target, duration=2):
+        """
+        Joint space goto
+        target : list of floats OR 4x4 homogeneous matrix
+            list : [shoulder_pitch, shoulder_roll, elbow_yaw, elbow_pitch, wrist_roll, wrist_pitch, wrist_yaw]
+            matrix : 4x4 matrix representing the target pose
+        """
+        # TODO implement duration
+
+        if target.shape == (4, 4):
+            # Compute IK etc
+            # target = self._control_ik.symbolic_ik_solver(target)
+            # 
+            # target = self._control_ik.symbolic_inverse_kinematics(name=self._prefix)
+            pass
+        elif len(target) == 7:
+            self.shoulder.pitch.goal_position = target[0]
+            self.shoulder.roll.goal_position = target[1]
+            self.elbow.yaw.goal_position = target[2]
+            self.elbow.pitch.goal_position = target[3]
+            self.wrist.roll.goal_position = target[4]
+            self.wrist.pitch.goal_position = target[5]
+            self.wrist.yaw.goal_position = target[6]
+        else:
+            raise ValueError("Invalid target shape")
+
+
